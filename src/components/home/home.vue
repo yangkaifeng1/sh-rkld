a70d9250aaa3d35340f9a7fd8a279b9682bda123
yangkaifeng <18809487346@163.com>
2018-08-06 00:23:27 +0800

    first commit

---
+++ src/components/home/home.vue
@@ -0,0 +1,133 @@
+<template>
+  <el-container class="home">
+    <el-header height="auto">
+      <el-card class="box-card">
+        <div slot="header" class="div-header clearfix">
+          <span>地区选择</span>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+          {{'列表内容 ' + o }}
+        </div>
+      </el-card>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu 
+          default-active="1"
+          class="el-menu-vertical-demo">
+          <el-menu-item index="1">
+            <i class="el-icon-setting"></i>
+            <span slot="title">本年度</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-setting"></i>
+            <span slot="title">半年</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">三个月</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">本月</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-setting"></i>
+            <span slot="title">本周</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">本日</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <el-row>
+          <el-col :span="16">
+            <div class="grid-content bg-purple">
+              <el-row>
+                <el-col :span="24">
+                  <div class="grid-content bg-purple-dark">
+                    流入流出统计
+                  </div>
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple">
+                    来源地总数TOP10
+                  </div>
+                </el-col>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple-light">
+                    流入人口年龄段统计
+                  </div>
+                </el-col>
+              </el-row>
+              <el-row>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple">
+                    流出地总数TOP10
+                  </div>
+                </el-col>
+                <el-col :span="12">
+                  <div class="grid-content bg-purple-light">
+                    流出人口年龄段统计
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </el-col>
+          <el-col :span="8">
+            <div class="grid-content bg-purple-light">
+              流入重点人员信息
+            </div>
+            <person-msg-s></person-msg-s>
+            <person-msg-s></person-msg-s>
+            <person-msg-s></person-msg-s>
+            <div class="grid-content bg-purple-light">
+              流出重点人员信息
+            </div>
+            <person-msg-s></person-msg-s>
+            <person-msg-s></person-msg-s>
+            <person-msg-s></person-msg-s>
+          </el-col>
+        </el-row>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+  import personMsgS from 'components/person-msg-s/person-msg-s'
+
+  export default {
+    name: 'Home',
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    components: {
+      personMsgS
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  .home{
+    .el-container{
+      padding: 20px 0 0 20px;
+    }
+    .el-main{
+      overflow: initial;
+      padding: 0 20px;
+    }
+    .el-row{
+      .el-col{
+        height: 50px;
+      }
+    }
+  }
+</style>
---
+++ src/components/person-msg-s/person-msg-s.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="person-msg-s">
+		<el-row :gutter="10">
+			<el-col :span="6">
+				<img src="./img/avatar.jpg" class="avatar">
+      </el-col>
+      <el-col :span="9">
+      	<el-row class="key-item">
+					<el-col :span="12">姓名：</el-col>
+		      <el-col :span="12">忆苦饭</el-col>
+		    </el-row>
+		    <el-row class="key-item">
+					<el-col :span="12">流入时间：</el-col>
+		      <el-col :span="12">2018-01-11</el-col>
+		    </el-row>
+		    <el-row class="key-item">
+					<el-col :span="12">标签信息：</el-col>
+		      <el-col :span="12">忆苦饭</el-col>
+		    </el-row>
+      </el-col>
+      <el-col :span="9">
+      	<el-row class="key-item">
+					<el-col :span="12">身份证：</el-col>
+		      <el-col :span="12">1111111111111</el-col>
+		    </el-row>
+		     <el-row class="key-item">
+					<el-col :span="12">户籍地址：</el-col>
+		      <el-col :span="12">甘肃省秦安先</el-col>
+		    </el-row>
+		    <el-row class="key-item get-detail">详情</el-row>
+      </el-col>
+    </el-row>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'personMsgS'
+	}
+</script>
+
+<style lang="less" scoped>
+	.person-msg-s{
+		padding: 5px 0;
+		font-size: 12px;
+		.avatar{
+			background: blue;
+			width: 100%;
+		}
+		.key-item{
+			padding: 10px 0;
+			&.get-detail{
+				color: #409eff; 
+				cursor: pointer;
+			}
+		}
+		.el-col{
+			text-overflow: ellipsis;
+  		overflow: hidden;
+ 			white-space: nowrap;
+		}
+		
+	}
+</style>
